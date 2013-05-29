aba43c421258d8d00bc7f2690816abbdefbfb22f
V. Glenn Tarcea <gtarcea@umich.edu>
2013-05-29 13:17:37 -0400

    More refactoring and code cleanup.

--- c_src/windows_if.c
+++ c_src/windows_if.c
@@ -40,16 +40,10 @@ static LPUSER_INFO_1 find_windows_user_entry(char *username)
 {
 	LPUSER_INFO_1 buf;
     wchar_t username_wide[32];
-    mbstowcs(username_wide, username, 32);
 
-    if (NetUserGetInfo(NULL, username_wide, (DWORD) 1, (LPBYTE *) &buf) == NERR_Success)
-    {
-        return buf;
-    }
-    else
-    {
-        return NULL;
-    }
+    mbstowcs(username_wide, username, 32);
+    return (NetUserGetInfo(NULL, username_wide, (DWORD) 1,
+                            (LPBYTE *) &buf) == NERR_Success) ? buf : NULL;
 }
 
 static struct handy_user *make_handy_user(LPUSER_INFO_1 lpuser)
