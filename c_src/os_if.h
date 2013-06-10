095f44cee2df13151e442cdb70db8fc0081401ed
V. Glenn Tarcea <gtarcea@umich.edu>
2013-06-10 17:07:44 -0400

    Wrap in #ifdef to prevent multiple includes.

--- c_src/os_if.h
+++ c_src/os_if.h
@@ -1,3 +1,6 @@
+#ifndef __OS_IF__H
+#define __OS_IF__H
+
 #include <string.h>
 
 #define MAX_PATH_SIZE 512
@@ -17,3 +20,5 @@ char *tmpdir();
 #ifdef _WIN32
 	char *realpath(const char *name, char *resolvedname);
 #endif
+
+#endif /* __OS_IF__H */
